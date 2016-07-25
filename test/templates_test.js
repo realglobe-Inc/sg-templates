f27754a22e345dad0d4900a6fa7643044e5de6b8
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-25 14:34:16 +0900

    Update template

--- test/templates_test.js
+++ test/templates_test.js
@@ -4,9 +4,10 @@
  */
 'use strict'
 
-const templates = require('../lib/templates.js')
+const Templates = require('../lib/templates.js')
 const assert = require('assert')
 const co = require('co')
+const coz = require('coz')
 
 describe('templates', function () {
   this.timeout(3000)
@@ -20,7 +21,22 @@ describe('templates', function () {
   }))
 
   it('Templates', () => co(function * () {
-
+    yield coz.render({
+      tmpl: Templates.moduleIndex,
+      force: true,
+      mkdirp: true,
+      mode: '444',
+      path: `${__dirname}/../tmp/testing-templates/index.js`,
+      data: {
+        pkg: {
+          name: 'hoge'
+        },
+        name: 'Fuge',
+        sub: {
+          foo: './foo'
+        }
+      }
+    })
   }))
 })
 
